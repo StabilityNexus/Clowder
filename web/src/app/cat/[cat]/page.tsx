cf41927dc2fa902577079542b39ab603fbbdf2f8
Anjali Jha <109814198+blizet@users.noreply.github.com>
2024-12-04 20:46:05 +0530

    Update page.tsx

--- web/src/app/cat/[cat]/page.tsx
+++ web/src/app/cat/[cat]/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Layout from "../../../components/Layout";
 
-// Page Component for the dynamic `[cat]` route
-export default function Page({ params }: { params: { cat: string } }) {
-  const { cat } = params; // Extract `cat` from `params`
+export default async function Page({
+  params,
+}: { params: Promise<{ cat: string }> }) {
+  const resolvedParams = await params;
   return (
     <Layout>
       <div className="justify-center text-center mt-9">
-        My Post: {cat}
+        My Post: {resolvedParams.cat}
       </div>
     </Layout>
   );
